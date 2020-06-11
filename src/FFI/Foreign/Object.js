f1fe4c6e780ae0b8a8b89a5a5e41955b2b1e2a1c
Mark Farrell <markfarr2011@gmail.com>
2020-06-11 21:49:41 +0000

    Add literal (FFI.Foreign.Object)

--- src/FFI/Foreign/Object.js
+++ src/FFI/Foreign/Object.js
@@ -3,3 +3,17 @@
 exports.values = Object.values;
 
 exports.keys = Object.keys;
+
+exports.literal_ = function(fst) {
+  return function(snd) {
+    return function(input) {
+      var output = {};
+      input.forEach(function(keyValue) {
+        var key = fst(keyValue);
+        var value = snd(keyValue);
+        output[key] = value;
+      });
+      return output;
+    };
+  };
+};
--- src/FFI/Foreign/Object.purs
+++ src/FFI/Foreign/Object.purs
@@ -1,10 +1,42 @@
 module FFI.Foreign.Object
   ( keys
   , values
+  , literal
+  , string
+  , number
+  , integer
+  , boolean
   ) where
 
+import Prelude
+
+import Data.Tuple (Tuple)
+import Data.Tuple as T
+
 import Foreign (Foreign)
+
+import Unsafe.Coerce as C
+
+foreign import literal_ :: (forall a b. Tuple a b -> a) -> (forall a b. Tuple a b -> b) -> Array (Tuple String Foreign) -> Foreign
 
 foreign import keys :: Foreign -> Array String
 
 foreign import values :: Foreign -> Array Foreign
+
+literal :: Array (Tuple String Foreign) -> Foreign
+literal = literal_ T.fst T.snd
+
+entry :: forall a. String -> a -> Tuple String Foreign
+entry k v = T.Tuple k $ C.unsafeCoerce v
+
+string :: String -> String -> Tuple String Foreign
+string = entry
+
+number :: String -> Number -> Tuple String Foreign
+number = entry
+
+integer :: String -> Int -> Tuple String Foreign
+integer = entry
+
+boolean :: String -> Boolean -> Tuple String Foreign
+boolean = entry
